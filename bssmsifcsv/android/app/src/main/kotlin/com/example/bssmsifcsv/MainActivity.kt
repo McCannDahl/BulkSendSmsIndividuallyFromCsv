331435a486d9ffea51f14a03aeaa6c9f3e9809fd
McCann Dahl <mccann.dahl@parivedasolutions.com>
2022-11-23 00:26:23 -0600

    created app

---
+++ bssmsifcsv/android/app/src/main/kotlin/com/example/bssmsifcsv/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bssmsifcsv
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bssmsifcsv/lib/main.dart
@@ -0,0 +1,159 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:csv/csv.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_sms/flutter_sms.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BSSMSIFCSV',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool filePicked = false;
+  List<String> phoneNumbers = [];
+  String? displayText;
+  late TextEditingController _controller;
+  bool messageExists = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+    getPermissions();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> getPermissions() async {
+    final permission = Permission.sms.request();
+    if (await permission.isGranted) {
+      pickFile();
+    }
+  }
+
+  Future<void> pickFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+    if (result != null && result.files.single.path != null) {
+      File file = File(result.files.single.path!);
+      setState(() {
+        filePicked = true;
+      });
+      parseFile(file);
+    }
+  }
+
+  bool isValidPhoneNumber(String? value) => RegExp(r'(^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$)').hasMatch(value ?? '');
+
+  Future<void> parseFile(File file) async {
+    final input = file.openRead();
+    final fields = await input.transform(utf8.decoder).transform(const CsvToListConverter()).toList();
+    for (var field in fields) {
+      try {
+        var phoneNumber = field[3].toString();
+        if (phoneNumber != null && phoneNumber != '') {
+          if (isValidPhoneNumber(phoneNumber)) {
+            phoneNumbers.add(phoneNumber);
+          }
+        }
+      } catch (err) {
+        print(err);
+      }
+    }
+    setState(() {
+      phoneNumbers = phoneNumbers;
+    });
+  }
+
+  Future<void> _sendTexts() async {
+    var result = '';
+    result = await sendSMS(message: _controller.text, recipients: phoneNumbers, sendDirect: true).catchError((onError) {
+      print(onError);
+      result = onError.toString();
+      return onError.toString();
+    });
+    setState(() {
+      displayText = result == '' ? 'Success' : result;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Hello!',
+            ),
+            !filePicked
+                ? const Text(
+                    'You must pick a file',
+                  )
+                : Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+                    Text(
+                      'You will send to ${phoneNumbers.length} numbers',
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      child: TextField(
+                        onChanged: (value) => setState(() {
+                          messageExists = value != '';
+                        }),
+                        controller: _controller,
+                        keyboardType: TextInputType.multiline,
+                        minLines: 1,
+                        maxLines: 5,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Message',
+                        ),
+                      ),
+                    ),
+                    displayText != null
+                        ? Text(
+                            'Result: $displayText',
+                          )
+                        : Container()
+                  ])
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: filePicked && messageExists ? _sendTexts : null, tooltip: 'Send', child: const Icon(Icons.send), backgroundColor: filePicked && messageExists ? null : Colors.grey), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
